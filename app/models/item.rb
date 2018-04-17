90377b3fe2ad5dcb27f63bf0185129bab4b6df9f
Thiago Belem <contato@thiagobelem.net>
2018-04-17 01:39:38 -0300

    Add Items to sidebar

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  RARITIES = %i(common uncommon rare epic legendary artifact).freeze
+
   # Validations
   validates :name, :icon, :kind, :value, presence: true
+
+  def rarity
+    RARITIES.sample
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+items = [
+  { name: 'Copper Ore', kind: 'ore', icon: 'inv_ore_copper_01', value: 10 },
+  { name: 'Iron Ore', kind: 'ore', icon: 'inv_ore_iron_01', value: 20 },
+  { name: 'Gold Ore', kind: 'ore', icon: 'inv_ore_gold_01', value: 30 },
+  { name: 'Raw Meat', kind: 'meat', icon: 'inv_misc_food_meat_rawtigersteak', value: 10 },
+  { name: 'Iron Sword', kind: 'weapon', icon: 'inv_sword_04', value: 50, stackable: false }
+]
+
+items.each { |item| Item.where(item).first_or_create! }
